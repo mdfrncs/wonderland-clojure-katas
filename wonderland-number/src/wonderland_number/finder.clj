9a9c5ccb0a5a0792d24ac3e15eb0d46357223fee
Michael Francis <mike.d.francis@gmail.com>
2016-07-22 15:22:52 -0300

    Wonderland number solution

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,13 @@
 (ns wonderland-number.finder)
 
+;;Copied from test code, but fixed since that version fails on inputs like [112 122]
+(defn same-digits? [n1 n2]
+  (let [s1 (sort (str n1))
+        s2 (sort (str n2))]
+    (= s1 s2)))
+
+(defn is-solution? [number]
+  (every? #(same-digits? number (* number %)) (range 2 6)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter is-solution? (range 100000 1000000))))
