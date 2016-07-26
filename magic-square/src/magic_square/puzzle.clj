f43b705e47e79de8eef26db62cc05bb9634f5e6b
Michael Francis <mike.d.francis@gmail.com>
2016-07-26 11:46:54 -0300

    Magic Square initial solution

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -2,7 +2,43 @@
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
+;; Copied from test code
+(defn sum-rows-prod [m]
+  (map #(reduce + %) m))
+
+(defn sum-cols-prod [m]
+  [(reduce + (map first m))
+   (reduce + (map second m))
+   (reduce + (map last m))])
+
+(defn sum-diagonals-prod [in]
+  (let [m (vec (map #(vec %) in))]
+    [(+ (get-in m [0 0]) (get-in m [1 1]) (get-in m [2 2]))
+     (+ (get-in m [2 0]) (get-in m [1 1]) (get-in m [0 2]))]))
+
+(defn magic-square? [values]
+  (and (= (set (sum-rows-prod values))
+          (set (sum-cols-prod values))
+          (set (sum-diagonals-prod values)))
+       (= 1
+          (count (set (sum-rows-prod values)))
+          (count (set (sum-cols-prod values)))
+          (count (set (sum-diagonals-prod values))))))
+
+(defn solnspace
+  ([values] (mapcat #(solnspace [] values %) values))
+  ([array symbols sym]
+   (let [to-process (remove #(= % sym) symbols)
+         soln (conj array sym)]
+     (if (= 2 (count to-process))
+       (solnspace soln to-process (first to-process) (last to-process))
+       (mapcat #(solnspace soln to-process %) to-process))
+     ))
+  ([array _ a b]
+   (list (conj array a b) (conj array b a))))
+
 (defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+  (vec (map vec (first
+    (filter magic-square?
+            (map #(vec (partition 3 %)) (solnspace values)))))))
+
