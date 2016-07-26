e2ee2c32c88ee9a9df8bcaf5ec033dcf20a714d0
Michael Francis <mike.d.francis@gmail.com>
2016-07-26 13:21:25 -0300

    Improved magic squares solution legibility

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -3,42 +3,44 @@
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
 ;; Copied from test code
-(defn sum-rows-prod [m]
+(defn- sum-rows [m]
   (map #(reduce + %) m))
 
-(defn sum-cols-prod [m]
+(defn- sum-cols [m]
   [(reduce + (map first m))
    (reduce + (map second m))
    (reduce + (map last m))])
 
-(defn sum-diagonals-prod [in]
+(defn- sum-diagonals [in]
   (let [m (vec (map #(vec %) in))]
     [(+ (get-in m [0 0]) (get-in m [1 1]) (get-in m [2 2]))
      (+ (get-in m [2 0]) (get-in m [1 1]) (get-in m [0 2]))]))
 
-(defn magic-square? [values]
-  (and (= (set (sum-rows-prod values))
-          (set (sum-cols-prod values))
-          (set (sum-diagonals-prod values)))
+(defn- magic-square? [values]
+  (and (= (set (sum-rows values))
+          (set (sum-cols values))
+          (set (sum-diagonals values)))
        (= 1
-          (count (set (sum-rows-prod values)))
-          (count (set (sum-cols-prod values)))
-          (count (set (sum-diagonals-prod values))))))
+          (count (set (sum-rows values)))
+          (count (set (sum-cols values)))
+          (count (set (sum-diagonals values))))))
 
-(defn solnspace
-  ([values] (mapcat #(solnspace [] values %) values))
-  ([array symbols sym]
-   (let [to-process (remove #(= % sym) symbols)
-         soln (conj array sym)]
-     (if (= 2 (count to-process))
-       (solnspace soln to-process (first to-process) (last to-process))
-       (mapcat #(solnspace soln to-process %) to-process))
-     ))
-  ([array _ a b]
-   (list (conj array a b) (conj array b a))))
+(defn permutations
+  [coll]
+  (if (empty? coll)
+    ['()]
+    (mapcat (fn [x]
+              (map #(conj % x)
+                   (permutations (remove #(= x %) coll))))
+         coll)))
+
+(defn- to-square
+  [value]
+  (vec (partition 3 value)))
 
 (defn magic-square [values]
-  (vec (map vec (first
-    (filter magic-square?
-            (map #(vec (partition 3 %)) (solnspace values)))))))
+  (mapv vec
+        (first
+          (filter magic-square?
+                  (map to-square (permutations values))))))
 
